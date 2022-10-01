0f46006084d8839c6d57be148f5b846bf2ff4112
Алексей Печин <organner@mail.ru>
2022-10-01 21:29:07 +0300

    Creating an interface PostRepository.java; Implementation of the interface PostRepository.java in the file PostRepositoryImpl.java; Creating a class PostService.java; Creating a class PostController.java

---
+++ src/main/java/org/example/controller/PostController.java
@@ -0,0 +1,44 @@
+package org.example.controller;
+
+import com.google.gson.Gson;
+import org.example.model.Post;
+import org.example.service.PostService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final Gson gson = new Gson();
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        deserialiseAndSerialise(response, service.all());
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        deserialiseAndSerialise(response, service.getById(id));
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        Post post = gson.fromJson(body, Post.class);
+        deserialiseAndSerialise(response, service.save(post));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        service.removeById(id);
+        deserialiseAndSerialise(response, "post with id=" + id + "delete");
+    }
+
+    private <T> void deserialiseAndSerialise(HttpServletResponse resp, T data) throws IOException {
+        resp.setContentType(APPLICATION_JSON);
+        String toJson = gson.toJson(data);
+        resp.getWriter().print(toJson);
+    }
+}
---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package org.example.repository;
+
+import org.example.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostRepository {
+
+    List<Post> all();
+    Optional<Post> getById(long id);
+    Post save(Post post);
+    boolean removeById(long id);
+}
